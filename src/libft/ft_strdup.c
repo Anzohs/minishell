80c1678b4d6a028c8a42ec2ecf80d84e694b7165
Your Name <teste@gmail.com>
2024-11-25 23:06:11 +0000

    ft_strdup

--- src/libft/ft_strdup.c
+++ src/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: malourei <malourei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 23:36:34 by malourei          #+#    #+#             */
-/*   Updated: 2024/11/25 22:14:51 by malourei         ###   ########.fr       */
+/*   Updated: 2024/11/25 23:05:18 by malourei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(char *s)
 	char	*temp;
 	int		i;
 
+	if (!s)
+		return (ft_calloc(1, 1));
 	temp = ft_calloc(sizeof(char), (ft_strlen(s) + 1));
 	if (!temp)
 		return (NULL);
