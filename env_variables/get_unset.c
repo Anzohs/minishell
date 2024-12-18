b3c28895a4dc89f3061b72e6d10e6754cc15445a
Manuel Gouveia <malourei@student.42lisboa.com>
2024-12-18 00:12:06 +0000

    testes ze

--- env_variables/get_unset.c
+++ env_variables/get_unset.c
@@ -6,24 +6,40 @@
 /*   By: malourei <malourei@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 20:53:15 by malourei          #+#    #+#             */
-/*   Updated: 2024/12/17 21:39:35 by malourei         ###   ########.fr       */
+/*   Updated: 2024/12/18 00:07:49 by malourei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static void	free_envs(t_mini *mini, int index)
+{
+	free(mini->super_env[index]);
+
+}
+
+
 static void	remove_env(t_mini *mini, t_node *node)
 {
 	int		i;
 	int		j;
+	int		index;
 	char	*str;
+	char	**strs;
 
-	i = 0;
 	str = node->entry.value;
 	// tenho que percorrer a str value para ler cada argumento para usar no unset
 	// e verificar se existe ou não para fazer o delete.
-	while(str[i])
+	strs = ft_split(str, ' ');
+	i = 0;
+	while (strs[i] != NULL)
 	{
+		index = get_index(mini->super_env, strs[i], ft_strlen(strs[i]));
+		if (index == -1)
+		{
+			i++;
+			continue ;
+		}
 
 	}
 }
