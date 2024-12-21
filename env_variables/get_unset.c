583f90f01ac807dead6e9127b97ef6225983390f
Manuel Gouveia <malourei@student.42lisboa.com>
2024-12-21 00:37:51 +0000

    unset vê ZÈ

--- env_variables/get_unset.c
+++ env_variables/get_unset.c
@@ -6,18 +6,13 @@
 /*   By: malourei <malourei@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 20:53:15 by malourei          #+#    #+#             */
-/*   Updated: 2024/12/19 20:52:07 by malourei         ###   ########.fr       */
+/*   Updated: 2024/12/21 00:36:13 by malourei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/* static int	is_exist(int *array, int number)
-{
-
-} */
-
-static	int	len_vars(char **strs)
+static int	len_vars(char **strs)
 {
 	int	i;
 
@@ -27,95 +22,99 @@ static	int	len_vars(char **strs)
 	return (i);
 }
 
-static char	**copy_new_env(t_mini *mini, int *array, int tam)
+static int	*init_array(char **strs, char **envs)
+{
+	int	i;
+	int	tam;
+	int	*array;
+
+	tam = len_vars(strs);
+	array = malloc(sizeof(int) * tam);
+	if (!array)
+	{
+		free_env(strs);
+		return (NULL);
+	}
+	i = 0;
+	while (tam > i)
+	{
+		array[i] = get_index(envs, strs[i], ft_strlen(strs[i]));
+		i++;
+	}
+	return (array);
+}
+
+static void	fill_array_index(int *array, int size, char **envs, int *count)
+{
+	int	i;
+
+	i = 0;
+	*count = 0;
+	while (i < size)
+	{
+		if (array[i] != -1)
+		{
+			envs[array[i]][0] = '\0';
+			(*count)++;
+		}
+		i++;
+	}
+}
+
+static char	**copy_new_env(t_mini *mini, int count_index)
 {
 	char	**new_env;
 	int		i;
 	int		j;
 
-	(void) array;
-	new_env = ft_calloc(sizeof(char *), len_vars(mini->super_env) - tam - 1);
+	new_env = ft_calloc(sizeof(char *), len_vars(mini->super_env) - count_index + 1);
 	if (!new_env)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (mini->super_env[i] != NULL)
 	{
-		if (mini->super_env[i][0] == '\0')
-			i++;
-		else if (mini->super_env[i][0] != '\0')
+		if (mini->super_env[i][0] != '\0')
 		{
 			new_env[j] = ft_strdup(mini->super_env[i]);
 			j++;
-			i++;
 		}
+		i++;
 	}
 	new_env[j] = NULL;
 	return (new_env);
 }
 
-static void	fill_array_index(int *array, char **envs, char **vars)
-{
-	int	i;
-	int	j;
-	int	index;
-
-	i = 0;
-	j = 0;
-	while (envs[i] != NULL)
-	{
-		index = get_index(envs, vars[j], ft_strlen(vars[j]));
-		if (index != -1)
-		{
-			envs[index][0] = '\0';
-			array[j] = index;
-			j++;
-		}
-		i++;
-	}
-}
-
-static void	init_array(int *array, int len, int *count)
-{
-	int	i;
-
-	i = 0;
-	count = 0;
-	while (len > 0)
-	{
-		array[i] = -1;
-		count++;
-		i++;
-	}
-}
-
 static void	remove_env(t_mini *mini, t_node *node)
 {
 	int		*array;
-	char	*str;
 	char	**strs;
 	int		i;
+	char	**new_env;
 
-	str = node->entry.value;
-	strs = ft_split(str, ' ');
+	strs = ft_split(((char *)node->entry.value), ' ');
 	if (!strs)
 		return ;
-	array = malloc(sizeof(int) * len_vars(strs));
+	array = init_array(strs, mini->super_env);
 	if (!array)
 		return ;
-	init_array(array, len_vars(strs), &i);
-	fill_array_index(array, mini->super_env, strs);
-	strs = copy_new_env(mini, array, i);
-	free(mini->super_env);
-	mini->super_env = strs;
+	fill_array_index(array, len_vars(strs), mini->super_env, &i);
+	new_env = copy_new_env(mini, i);
+	if (!new_env)
+	{
+		free(array);
+		free_env(strs);
+		return ;
+	}
+	free_env(mini->super_env);
+	mini->super_env = new_env;
+	free(array);
+	free_env(strs);
 }
-
-
 
 void	get_unset(t_mini *mini, t_node *command)
 {
-	//(void) mini;
-	//printf("%s\n", (char *)command->entry.value);
-	// ver o porquê do segfault mesmo quando não tem VAR a pesquisar
+	if (((char *)command->entry.value)[0] == '\0')
+		return ;
 	remove_env(mini, command);
 }
