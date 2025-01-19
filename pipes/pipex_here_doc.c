57bf9ef266a50adf8b328263558a1037fd70a163
Manuel Gouveia <malourei@student.42lisboa.com>
2025-01-19 23:04:14 +0000

    start <<

--- pipes/pipex_here_doc.c
+++ pipes/pipex_here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_here_doc.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malourei <malourei@student.42.com>         +#+  +:+       +#+        */
+/*   By: essmpt <essmpt@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:52:43 by malourei          #+#    #+#             */
-/*   Updated: 2025/01/10 17:09:24 by malourei         ###   ########.fr       */
+/*   Updated: 2025/01/19 23:02:40 by essmpt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,14 @@ void	ft_child_doc_one(t_pipex *pipex, char **env, char *cmd_path, t_node *n)
 	exit(0);
 }
 
-static void	here_doc(char *limiter, int fd[2], t_pipex *pipex)
+static void	here_doc(char *limiter, int fd[2])
 {
 	char	*line;
 
+	ft_close(fd[0]);
 	while (1)
 	{
-		write(1, "here_doc> ", 10);
+		write(1, "> ", 2);
 		line = get_next_line(0);
 		if (line == NULL)
 			break ;
@@ -52,27 +53,31 @@ static void	here_doc(char *limiter, int fd[2], t_pipex *pipex)
 		write(fd[1], line, ft_strlen(line));
 		free(line);
 	}
-	ft_close(fd[0]);
 	ft_close(fd[1]);
-	clean_all(pipex);
 }
 
-void	start_here_doc(t_pipex *pipex, t_node *n)
+int	start_here_doc(t_pipex *pipex, t_node *n)
 {
-	pipex->i_multi_argv = 2;
-	pipex->is_doc = 1;
-	if (pipe(pipex->fds[0].fd) < 0)
+	int pidefd[2];
+	int	pid;
+
+	if (pipe(pidefd) < 0)
 	{
 		perror("pipe1");
 		return ;
 	}
-	pipex->pids[0] = fork();
-	if (pipex->pids[0] < 0)
+	pid = fork();
+	if (pid < 0)
 	{
 		perror("pid3");
 		return ;
 	}
-	if (pipex->pids[0] == 0)
-		here_doc(n->entry.value, pipex->fds[0].fd, pipex);
-	ft_close(pipex->fds[0].fd[1]);
+	if (pid == 0)
+		here_doc(n->entry.value, pidefd);
+	else
+	{
+		ft_close(pidefd[1]);
+		waitpid(pid, NULL , 0);
+		return (pidefd[0]);
+	}
 }
