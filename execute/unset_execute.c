b5eebb754362935e9469e4a321198dd354d007b2
Hugo Emanuel Ladeiro Nobre <10549215+Anzohs@users.noreply.github.com>
2025-02-07 11:00:59 +0000

    Update unset_execute.c

--- execute/unset_execute.c
+++ execute/unset_execute.c
@@ -3,7 +3,22 @@
 
 void    unset_execute(t_cmd *cmd)
 {
+    int i;
+    t_list *env_node;
+    t_list *exp_node;
+
     if (!cmd->matrix || !*cmd->matrix[0])
         return ;
-    
+    {
+
+    i = -1;
+    while (cmd->matrix[++i])
+    {
+        env_node = ft_lstgetlst(&mini()->env, cmd->matrix[i]);
+        if (env_node)
+            ft_lstpop(&mini()->env, env_node);
+        exp_node = ft_lstgetlst(&mini()->exp, cmd->matrix[i]);
+        if (exp_node)
+            ft_lstpop(&mini()->exp, exp_node);
+    }
 }
