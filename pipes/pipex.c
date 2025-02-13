fd70c963ae270eb14fd294c3fc6e17fca9730208
Manuel Gouveia <malourei@student.42lisboa.com>
2025-02-13 22:24:29 +0000

    zee olha os redi e pipes

--- pipes/pipex.c
+++ pipes/pipex.c
@@ -6,7 +6,7 @@
 /*   By: malourei <malourei@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 20:24:53 by essmpt            #+#    #+#             */
-/*   Updated: 2025/02/12 01:16:14 by malourei         ###   ########.fr       */
+/*   Updated: 2025/02/13 22:17:31 by malourei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,22 +38,29 @@ static void	start_multi2_pip(t_pipex *pipex, int i, char *cmd_path, t_cmd *node)
 		return (perror("pid"), free(pipex->pids), (void)i);
 	if (pipex->pids[i] == 0)
 	{
-		if (dup2(pipex->fds[i - 1].fd[0], STDIN_FILENO) < 0)
-			return (perror("dup5"), (void)i);
-		if (dup2(pipex->fds[i].fd[1], STDOUT_FILENO) < 0)
-			return (perror("dup6"), (void)i);
 		if (node->fd)
 		{
-			if (node->w == 3)
+			if (node->w >= 3)
 			{
+				if (dup2(pipex->fds[i].fd[1], STDOUT_FILENO) < 0)
+					return (perror("dup6"), (void)i);
 				dup2(node->w, STDOUT_FILENO);
 				ft_close(node->w);
 			}
-			else if (node->read == 3)
+			else if (node->read >= 3)
 			{
 				dup2(node->read, STDIN_FILENO);
 				ft_close(node->read);
+				if (dup2(pipex->fds[i].fd[1], STDOUT_FILENO) < 0)
+					return (perror("dup6"), (void)i);
 			}
+		}
+		else
+		{
+			if (dup2(pipex->fds[i - 1].fd[0], STDIN_FILENO) < 0)
+				return (perror("dup5"), (void)i);
+			if (dup2(pipex->fds[i].fd[1], STDOUT_FILENO) < 0)
+				return (perror("dup6"), (void)i);
 		}
 		ft_close_all_m(pipex, i);
 		execve2(cmd_path, node, pipex->env);
